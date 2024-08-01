1cf2b65f3facda75c0c16b5415fd120b0c710d96
yebrong <officeguider@naver.com>
2024-08-01 15:51:25 +0900

    Initial commit

---
+++ src/main/java/com/cloudians/CloudiansApplication.java
@@ -0,0 +1,13 @@
+package com.cloudians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudiansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudiansApplication.class, args);
+	}
+
+}

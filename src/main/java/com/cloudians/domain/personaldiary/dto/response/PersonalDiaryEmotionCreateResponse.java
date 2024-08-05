74690084718b8a1d589dc0003b43262b1a44b26f
Jiihyun <cwlgusc@gmail.com>
2024-08-05 14:38:21 +0900

    feat: PersonalDiaryEmotionCreateResponse에 createdAt, updatedAt 추가

--- src/main/java/com/cloudians/domain/personaldiary/dto/response/PersonalDiaryEmotionCreateResponse.java
+++ src/main/java/com/cloudians/domain/personaldiary/dto/response/PersonalDiaryEmotionCreateResponse.java
@@ -4,9 +4,11 @@ import com.cloudians.domain.personaldiary.entity.PersonalDiaryEmotion;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PersonalDiaryEmotionCreateResponse {
-    private Long id;
+    private Long emotionId;
 
     private int joy;
 
@@ -18,24 +20,32 @@ public class PersonalDiaryEmotionCreateResponse {
 
     private int boredom;
 
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
     @Builder
-    private PersonalDiaryEmotionCreateResponse(Long id, int joy, int sadness, int anger, int anxiety, int boredom) {
-        this.id = id;
+    private PersonalDiaryEmotionCreateResponse(Long emotionId, int joy, int sadness, int anger, int anxiety, int boredom, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.emotionId = emotionId;
         this.joy = joy;
         this.sadness = sadness;
         this.anger = anger;
         this.anxiety = anxiety;
         this.boredom = boredom;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
     public static PersonalDiaryEmotionCreateResponse of(PersonalDiaryEmotion personalDiaryEmotion) {
         return PersonalDiaryEmotionCreateResponse.builder()
-                .id(personalDiaryEmotion.getId())
+                .emotionId(personalDiaryEmotion.getId())
                 .joy(personalDiaryEmotion.getJoy())
                 .sadness(personalDiaryEmotion.getSadness())
                 .anger(personalDiaryEmotion.getAnger())
                 .anxiety(personalDiaryEmotion.getAnxiety())
                 .boredom(personalDiaryEmotion.getBoredom())
+                .createdAt(personalDiaryEmotion.getCreatedAt())
+                .updatedAt(personalDiaryEmotion.getUpdatedAt())
                 .build();
     }
 }

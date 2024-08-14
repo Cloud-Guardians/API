64a77d79b604ebca302eedc832101fd1b9af43e2
yebrong <officeguider@naver.com>
2024-08-14 14:06:48 +0900

    fix: storage and exception type

---
+++ cloudians/src/main/java/com/cloudians/domain/publicdiary/dto/request/shell.java
@@ -0,0 +1,5 @@
+package com.cloudians.domain.publicdiary.dto.request;
+
+public interface shell {
+
+}
---
+++ cloudians/src/main/java/com/cloudians/domain/statistics/entity/shell.java
@@ -0,0 +1,5 @@
+package com.cloudians.domain.statistics.entity;
+
+public interface shell {
+
+}

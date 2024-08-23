2128ea25f9553e4e0ba0bda45c643973055e0cdc
Jiihyun <cwlgusc@gmail.com>
2024-08-23 21:05:45 +0900

    refactor: searchType, reportReason string->enum 예외 추가

--- src/main/java/com/cloudians/domain/publicdiary/exception/PublicDiaryExceptionType.java
+++ src/main/java/com/cloudians/domain/publicdiary/exception/PublicDiaryExceptionType.java
@@ -11,8 +11,8 @@ public enum PublicDiaryExceptionType implements BaseExceptionType {
     BAD_REQUEST(400, HttpStatus.BAD_REQUEST, "잘못된 요청건입니다."),
     FORBIDDEN_USER(403, HttpStatus.FORBIDDEN, "접근 권한이 없는 사용자입니다."),
     SELF_REPORT(400, HttpStatus.BAD_REQUEST, "신고 대상이 올바르지 않습니다."),
-    ALREADY_REPORT(400, HttpStatus.BAD_REQUEST, "이미 신고한 게시글입니다.");
-
+    ALREADY_REPORT(400, HttpStatus.BAD_REQUEST, "이미 신고한 게시글입니다."),
+    WRONG_REPORT_REASON(400, HttpStatus.BAD_REQUEST, "신고 사유가 올바르지 않습니다.");
 
     private final int statusCode;
     private final HttpStatus httpStatus;
